b93e55dca73033cc671432a1e4c3b80063ebc851
sovea <harleyhhh423@gmail.com>
2021-06-15 10:30:00 +0800

    change error message

--- main.go
+++ main.go
@@ -29,13 +29,13 @@ func main() {
 	instance_loading := module_checking.InitialModel("Network Check Task Dispatching ...", spinner.Points)
 	program_net_check := tea.NewProgram(instance_loading)
 	if err := program_net_check.Start(); err != nil {
-		fmt.Printf("Could not start program: %v", err)
+		fmt.Printf("Could not start program...\n\n Please check user permissions and network status, etc...")
 		os.Exit(1)
 	}
 	instance_chose_env := module_chose_env.InitialModel()
 	program_chose_env := tea.NewProgram(instance_chose_env)
 	if err := program_chose_env.Start(); err != nil {
-		fmt.Printf("Could not start program: %v", err)
+		fmt.Printf("Could not start ssh handle program... \n\n Please check user permissions, software source and network status, etc...")
 		os.Exit(1)
 	}
 }
